e75c68b386571fcf7cb188cd2e2124080f257347
Joey Hurley <joeyhurley@gmail.com>
2025-03-12 17:11:53 -0500

    final bookbot with parameters for book file path

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from stats import get_num_words
 from stats import get_num_letters
 from stats import sorted_list
+import sys
 
 def main():
-    book_text = get_book_text()
+
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    book_text = get_book_text(path)
     word_count = get_num_words(book_text)
     Letter_count = get_num_letters(book_text)
     sorterd_list_dict = sorted_list(Letter_count)
    
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -22,8 +29,8 @@ def main():
     print("============= END ===============")
 
 
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(path):
+    with open(path) as f:
         file_contents = f.read()
     return file_contents
 
