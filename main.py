17f206789a905ffa1429500811504ee5b0d6d279
Joey Hurley <joeyhurley@gmail.com>
2025-03-12 16:38:03 -0500

    bookbot with stats and formated report

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from stats import get_num_words
+from stats import get_num_letters
+from stats import sorted_list
 
 def main():
     book_text = get_book_text()
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
+    Letter_count = get_num_letters(book_text)
+    sorterd_list_dict = sorted_list(Letter_count)
+   
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorterd_list_dict:
+        char = item["char"]
+        count = item["count"]
+        if char.isalpha():
+            print(f":{char}: {count}")
+
+    print("============= END ===============")
+
 
 def get_book_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     return file_contents
+
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_num_letters(text):
+    letters = {}
+    for l in text.lower():
+        if l in letters:
+            letters[l] += 1
+        else:
+            letters[l] = 1
+    return letters
+
+def sort_on(dict):
+    return dict["count"]
+
+def sorted_list(char_dict):
+    # Create a list of dictionaries
+    chars_list = []
+    for char, count in char_dict.items():
+        chars_list.append({"char": char, "count": count})
+    # Sort the list
+    chars_list.sort(key=sort_on, reverse=True)
+    return chars_list
